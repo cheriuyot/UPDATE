3b8d2fd76250c0edb2cd4c3dd54c3172725332fd
unknown <Kevin.Marete@VDI-HP-LAB-5.strathmore.local>
2014-11-10 20:12:38 +0300

    latest hash

--- application/config/github_updater.php
+++ application/config/github_updater.php
@@ -21,7 +21,7 @@ $config['github_branch'] = 'master';
  * NOTE: You should only need to set this initially it will be
  * automatically set by the library after subsequent updates.
  */
-$config['current_commit'] = '51820140a0a2d5d41aa26e8c3806f8085e558251';
+$config['current_commit'] = 'eb4a042b8f63a2fb6a5c525158127ae87ccb4aab';
 
 /**
  * A list of files or folders to never perform an update on.
